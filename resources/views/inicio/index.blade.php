0bff82ebfc2fbf027dda9347dddf0baa0e0e0192
Rodrigues Mafumo <mafumorodrigues@gmail.com>
2017-11-17 13:39:38 -0800

    index finalizado

--- resources/views/inicio/index.blade.php
+++ resources/views/inicio/index.blade.php
@@ -59,22 +59,8 @@
    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDnOio2-38aqc86jOFG4soPgvx6lVDIPRc&libraries=places"></script>
     
     <script type="text/javascript">
-      google.maps.event.addDomListener(window,'load',autocompletar);
-
-      function autocompletar(){
-      var auto_complete=new google.maps.places.Autocomplete(document.getElementById('txt_autocomplete'));
-
-      google.maps.events.AddListener(autocomplete,'plac_changed',function(){
-
-        var places=autocomplete.getPlace();
-        var location='<b>Location:</b>'+places.formatted_address+"<br>";
-
-        location+="<b>Latitude:<b>"+places.geometry.location.A+"<br>";
-
-           location+="<b>Longitude:<b>"+places.geometry.location.F+"<br>";
-
+      google.maps.event.addDomListener(window,'load',function(){
+        var input=document.getElementById('txt_autocomplete');
+        var auto=new google.maps.places.Autocomplete(input);
       });
-
-      document.getElementById('txt_endereco').innerHtml=location;
-
     </script>
